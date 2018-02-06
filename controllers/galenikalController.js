bf47823cc665eb393b0e8b8d0bf339b814c75c8b
fetreney2000 <36126302+fetreney2000@users.noreply.github.com>
2018-02-06 13:34:14 +0800

    Update galenikalController.js

--- controllers/galenikalController.js
+++ controllers/galenikalController.js
@@ -1,1 +1,19 @@
+exports.extemporaneous = (req, res) => {
+    res.render('user/galenikal/internal/extemporaneous');
+};
 
+exports.pukal = (req, res) => {
+    res.render('user/galenikal/internal/pukal');
+};
+
+exports.losyen = (req, res) => {
+    res.render('user/galenikal/eksternal/losyen');
+};
+
+exports.krim = (req, res) => {
+    res.render('user/galenikal/eksternal/krim');
+};
+
+exports.statistik = (req, res) => {
+    res.render('user/galenikal/statistik');
+};
